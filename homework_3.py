847959ecf0194c0ef3f98f57dc4e566d506a3425
Yuliia Pyshnenko <yuliia@example.com>
2023-07-27 12:48:31 +0300

    added homework_3, if/else, collections

--- homework_3.py
+++ homework_3.py
@@ -1,7 +1,6 @@
 input_string = input("Enter ")
-set_from_string = set(input_string)
-unique_symbols = len(set_from_string)
-
+set_from_input = set(input_string)
+unique_symbols = len(set_from_input)
 print(unique_symbols)
 
 if unique_symbols > 10:
